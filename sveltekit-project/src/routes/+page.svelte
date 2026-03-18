000653dda5fb969e34652d6c17a427b67c873ffb
Phil Jacklin <phil.jacklin@thankyoupayroll.nz>
2026-03-19 11:12:18 +1300

    Put component onto a page.

--- sveltekit-project/src/routes/+page.svelte
+++ sveltekit-project/src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script lang="ts">
+    import { Button } from '/components/ui/Button';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<div class="mt-4">
+    <Button variant="default">Click me</Button>
+</div>
